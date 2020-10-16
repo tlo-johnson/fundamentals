fe7bcf9451f822f018a4c154194e7f7df240a818
Tolu Adesegha <tolu@adesegha.ca>
2020-10-16 03:20:26 -0600

    tried implementing juggling approach - not quite right yet

--- array/array-rotation.js
+++ array/array-rotation.js
@@ -18,6 +18,7 @@ const rotateWithTempArray = (arr, d) => {
   for (let index = 0; index < d; index++) temp.push(arr[index]);
   return temp;
 };
+console.log("rotating with array");
 console.log(rotateWithTempArray([1, 2, 3, 4, 5, 6, 7], 2));
 console.log(rotateWithTempArray([1, 2, 3, 4, 5, 6, 7], 4));
 
@@ -32,10 +33,45 @@ const rotateWithSubArray = (arr, d) => {
   for (let index = d; index < arr.length; index++) arr[index - d] = arr[index];
   return [...arr.slice(0, arr.length - d), ...temp];
 };
+console.log("rotating with sub array");
 console.log(rotateWithSubArray([1, 2, 3, 4, 5, 6, 7], 2));
 console.log(rotateWithSubArray([1, 2, 3, 4, 5, 6, 7], 4));
 
 /*
  * recursive / functional approach
+ * time complexity: O(n * d) - you repeat d times and each time you repeat, you go through the entire array
+ * space complexity: O(1)
  */
-const rotateRecursive = (arr, d) => {};
+const rotateRecursive = (arr, d) => {
+  if (d === 0) return arr;
+
+  // rotate array by 1
+  const firstElement = arr[0];
+  for (let index = 1; index < arr.length; index++) arr[index - 1] = arr[index];
+  arr[arr.length - 1] = firstElement;
+
+  return rotateRecursive(arr, --d);
+};
+console.log("rotating recursive");
+console.log(rotateRecursive([1, 2, 3, 4, 5, 6, 7], 2));
+console.log(rotateRecursive([1, 2, 3, 4, 5, 6, 7], 4));
+
+/*
+ * juggling approach
+ */
+const rotateJuggle = (arr, d) => {
+  for (let count = 0; count < d; count++) {
+    // starting with base element, rotate elements that are d elements away
+    const temp = arr[count];
+    let index = count;
+    for (; index < arr.length; index += d) {
+      if (!(index + d < arr.length)) break;
+      arr[index] = arr[index + d];
+    }
+    arr[index] = temp;
+  }
+  return arr;
+};
+console.log("rotating juggle");
+console.log(rotateJuggle([1, 2, 3, 4, 5, 6, 7], 2));
+console.log(rotateJuggle([1, 2, 3, 4, 5, 6, 7], 4));
