fcf67e18785462d3f75f59159f157111b11d5466
Tolu Adesegha <tolu@adesegha.ca>
2021-01-18 07:57:51 -0700

    solved max-consecutive ones - simplified implementation

--- leetcode/max-consecutive-ones/main.c
+++ leetcode/max-consecutive-ones/main.c
@@ -3,26 +3,23 @@
 int findMaxConsecutiveOnes(int* nums, int numsSize) {
   int max = 0;
   int count = 0;
-  int currentlyCounting = 0;
 
   while (--numsSize >= 0) {
-    int num = nums[numsSize];
-    if (num == 1) {
+    if (nums[numsSize] == 1) {
       count++;
-      currentlyCounting = 1;
     }
-    else if (currentlyCounting) {
-      if (count > max) max = count;
-      count = currentlyCounting = 0;
+    else {
+      count = 0;
     }
+
+    if (count > max) max = count;
   }
 
-  if (currentlyCounting && count > max) max = count;
   return max;
 }
 
 int main() {
-  int nums[] = { 0, 0, 0, 0, 0, 0 };
+  int nums[] = { 1, 1, 1, 1, 0, 0 };
   printf("max consecutive ones: %d\n", findMaxConsecutiveOnes(nums, sizeof(nums) / sizeof(int)));
   return 0;
 }
