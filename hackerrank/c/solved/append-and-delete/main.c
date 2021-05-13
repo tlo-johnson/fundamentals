a2452553ba9f98339831c5e1fbb0ae33ad74f2fc
Tolu Adesegha <tolu@adesegha.ca>
2021-05-13 07:38:07 -0600

    solve append and delete

---
+++ hackerrank/c/solved/append-and-delete/main.c
@@ -0,0 +1,180 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'appendAndDelete' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. STRING t
+ *  3. INTEGER k
+ */
+
+/*
+ * To return the string from the function, you should either do static allocation or dynamic allocation
+ *
+ * For example,
+ * char* return_string_using_static_allocation() {
+ *     static char s[] = "static allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ * char* return_string_using_dynamic_allocation() {
+ *     char* s = malloc(100 * sizeof(char));
+ *
+ *     s = "dynamic allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ */
+char* appendAndDelete(char* s, char* t, int k) {
+  // need to be able to get to start of diff in strings
+  // once at start of diff, need enough ops to be able to print rest of target string
+  // once strings are same, if leftover num ops is even, return yes
+  // if leftover num ops is odd and enough ops to delete the entire source and recreate target, return yes
+  // return no
+
+  int diff_index = 0;
+  int length_s = strlen(s);
+  int length_t = strlen(t);
+  int shorter_length = length_s < length_t ? length_s : length_t;
+  while (s[diff_index] == t[diff_index] && diff_index < shorter_length)
+    diff_index++;
+
+  int min_delete_ops = length_s - diff_index;
+  int min_add_ops = length_t - diff_index;
+  int remaining_ops = k - min_delete_ops - min_add_ops;
+
+  return (remaining_ops >= 0 && (remaining_ops % 2 == 0 || remaining_ops >= diff_index * 2)) ? "Yes" : "No";
+}
+
+int main()
+{
+    char* s = readline();
+
+    char* t = readline();
+
+    int k = parse_int(ltrim(rtrim(readline())));
+
+    char* result = appendAndDelete(s, t, k);
+
+    printf("%s\n", result);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
