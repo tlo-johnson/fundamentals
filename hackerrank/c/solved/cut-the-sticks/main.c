9e7e22858df3f457b3b2d537974c7e5f9bca648d
Tolu Adesegha <tolu@adesegha.ca>
2021-05-14 00:53:31 -0600

    solve cut the sticks

---
+++ hackerrank/c/solved/cut-the-sticks/main.c
@@ -0,0 +1,251 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'cutTheSticks' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+/*
+ * To return the integer array from the function, you should:
+ *     - Store the size of the array to be returned in the result_count variable
+ *     - Allocate the array statically or dynamically
+ *
+ * For example,
+ * int* return_integer_array_using_static_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     static int a[5] = {1, 2, 3, 4, 5};
+ *
+ *     return a;
+ * }
+ *
+ * int* return_integer_array_using_dynamic_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     int *a = malloc(5 * sizeof(int));
+ *
+ *     for (int i = 0; i < 5; i++) {
+ *         *(a + i) = i + 1;
+ *     }
+ *
+ *     return a;
+ * }
+ *
+ */
+int* cutTheSticks2(int arr_count, int* arr, int* result_count) {
+  int* result = malloc(arr_count * sizeof(int));
+  int min, previous_min = 0, r_count = 0, num_cuts;
+
+  do {
+    num_cuts = 0;
+    min = 1000;
+    for (int count = 0; count < arr_count; count++) {
+      int val = arr[count];
+      if (val > previous_min) {
+        num_cuts++;
+        if (val < min)
+          min = val;
+      }
+    }
+
+    if (num_cuts != 0)
+      result[r_count++] = num_cuts;
+    previous_min = min;
+  } while (num_cuts != 0);
+
+  *result_count = r_count;
+  return result;
+}
+
+int* cutTheSticks(int arr_count, int* arr, int* result_count) {
+  int* num_cuts = calloc(1001, sizeof(int));
+  int count = 0;
+
+  for (int index = 0; index < arr_count; index++) {
+    int val = arr[index];
+    if (num_cuts[val] == 0) count++;
+    num_cuts[val]++;
+  }
+
+  *result_count = count;
+
+  int* result = malloc(count * sizeof(int));
+  int sum = 0;
+  for (int index = 1000; index > 0; index--) {
+    int cuts = num_cuts[index];
+    sum += cuts;
+    if (cuts != 0)
+      result[--count] = sum;
+  }
+
+  free(num_cuts);
+  return result;
+}
+
+int main()
+{
+    int n = parse_int(ltrim(rtrim(readline())));
+
+    char** arr_temp = split_string(rtrim(readline()));
+
+    int* arr = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = parse_int(*(arr_temp + i));
+
+        *(arr + i) = arr_item;
+    }
+
+    int result_count;
+    int* result = cutTheSticks(n, arr, &result_count);
+
+    for (int i = 0; i < result_count; i++) {
+        printf("%d", *(result + i));
+
+        if (i != result_count - 1) {
+            printf("\n");
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
