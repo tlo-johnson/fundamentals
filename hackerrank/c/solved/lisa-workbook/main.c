feff2727afe860611b0f94d1018beb1e5be045b0
Tolu Adesegha <tolu@adesegha.ca>
2021-05-24 07:15:08 -0600

    solve lisa workbook

---
+++ hackerrank/c/solved/lisa-workbook/main.c
@@ -0,0 +1,204 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+char** split_string(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'workbook' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY arr
+ */
+
+/*
+ * page 1: 1 - num_problems_per_page
+ * page 2: (num_problems_per_page + 1) - (num_problems_per_page * 2)
+ * page 3: ((num_problems_per_page * 2) + 1) - (num_problems_per_page * 3)
+ *
+ * page 4: 1 - num_problems_per_page
+ */
+int workbook(int num_chapters, int num_problems_per_page, int arr_count, int* arr) {
+  int special_problems = 0;
+  int page = 1;
+
+  for (int index = 0; index < arr_count; index++) {
+    int num_problems = arr[index];
+    int chapter_page_count = 0;
+    while (num_problems > 0) {
+      int lowest_problem_on_page = (num_problems_per_page * chapter_page_count) + 1;
+      int highest_problem_on_page = num_problems > num_problems_per_page
+        ? num_problems_per_page * (chapter_page_count + 1)
+        : lowest_problem_on_page + num_problems - 1;
+
+      if (page <= highest_problem_on_page && page >= lowest_problem_on_page)
+        special_problems++;
+
+      num_problems -= num_problems_per_page;
+      chapter_page_count++;
+      page++;
+    }
+  }
+
+  return special_problems;
+}
+
+int main()
+{
+    char** first_multiple_input = split_string(rtrim(readline()));
+
+    int n = parse_int(*(first_multiple_input + 0));
+
+    int k = parse_int(*(first_multiple_input + 1));
+
+    char** arr_temp = split_string(rtrim(readline()));
+
+    int* arr = malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = parse_int(*(arr_temp + i));
+
+        *(arr + i) = arr_item;
+    }
+
+    int result = workbook(n, k, n, arr);
+
+    printf("%d\n", result);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
